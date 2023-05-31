64b2968cfaa941648fc3bb741053c6f116ee2153
TOVTC <veronica.tc.to@outlook.com>
2023-05-31 14:17:05 -0400

    added validation for transactions

--- Classes/BankAccount.cs
+++ Classes/BankAccount.cs
@@ -8,23 +8,54 @@ public class BankAccount
 
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; set; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
 
     public BankAccount(string name, decimal initialBalance)
     {
-        this.Owner = name;
-        this.Balance = initialBalance;
-        this.Number = _accountNumberSeed.ToString();
+        Number = _accountNumberSeed.ToString();
         _accountNumberSeed++;
+
+        Owner = name;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
     }
+
+    private List<Transaction> _allTransactions = new List<Transaction>();
+
+    // The throw statement throws an exception.
+    // Execution of the current block ends, and control transfers to the first matching catch block found in the call stack. 
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
-
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be greater than 0");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
-
+        if (amount <=0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be greater than 0");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Insufficient funds for this withdrwawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
     }
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,4 +3,30 @@
 var account = new BankAccount("Ronnie", 1000);
 var account2 = new BankAccount("Shun", 10000);
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
 Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
+
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+try
+{
+    BankAccount invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance"); ;
+    Console.WriteLine(e.ToString());
+    return;
+}
